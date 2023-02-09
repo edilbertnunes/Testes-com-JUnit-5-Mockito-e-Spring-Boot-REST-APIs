77e5e8c09d45982ceb21567332ba780962dc8e6b
Edilbert Nunes <edilbert.nunes@gmail.com>
2023-02-09 16:10:36 -0300

    teste para manipulador de excecoes metodo data integrity

--- src/test/java/br/com/dicasdeumdev/api/resources/exceptions/ResourceExceptionHandleTest.java
+++ src/test/java/br/com/dicasdeumdev/api/resources/exceptions/ResourceExceptionHandleTest.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.api.resources.exceptions;
 
+import br.com.dicasdeumdev.api.services.exceptions.DataIntegratyViolationException;
 import br.com.dicasdeumdev.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -35,11 +36,23 @@ class ResourceExceptionHandleTest {
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
         assertEquals(ResponseEntity.class, response.getClass());
         assertEquals(StandardError.class, response.getBody().getClass());
-        assertEquals("Objeto não encontado", response.getBody().getError());
+        assertEquals("Objeto não encontrado", response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = exceptionHandle
+                .dataIntegrityViolationException(
+                        new DataIntegratyViolationException("E-mail já cadastrado"),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("E-mail já cadastrado", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
     }
 }
