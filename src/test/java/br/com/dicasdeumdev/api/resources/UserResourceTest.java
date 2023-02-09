180aca1cbbc2f2b6d654c7b2825548042e5e2eaa
Edilbert Nunes <edilbert.nunes@gmail.com>
2023-02-09 14:35:17 -0300

    classe de testes para user resource iniciada

---
+++ src/test/java/br/com/dicasdeumdev/api/resources/UserResourceTest.java
@@ -0,0 +1,72 @@
+package br.com.dicasdeumdev.api.resources;
+
+import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDto;
+import br.com.dicasdeumdev.api.services.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class UserResourceTest {
+
+    public static final Integer ID      = 1;
+    public static final String NAME     = "edilbert";
+    public static final String EMAIL    = "email@email";
+    public static final String PASSWORD = "123";
+
+    private User user;
+    private UserDto userDto;
+
+    @InjectMocks
+    private UserResource resource;
+
+    @Mock
+    private ModelMapper mapper;
+    @Mock
+    private UserServiceImpl service;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDto = new UserDto(ID, NAME, EMAIL, PASSWORD);
+    }
+}
